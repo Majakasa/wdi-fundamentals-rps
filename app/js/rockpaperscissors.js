0109d5a75aa5d61befbd0bc567b50a8f5a032da5
Malcolm J Sackler <mal@Malcolm-Sacklers-Computer.local>
2016-01-20 02:13:56 -0500

    added getWinner

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -23,22 +23,43 @@ function randomPlay() {
 
 function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
-    move = move || getInput()
+    
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return (move = move || getInput())/* Your Expression */;
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
-    move = move || randomPlay()
+    
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return (move = move || randomPlay()) /* Your Expression */;
 }
 
 function getWinner(playerMove,computerMove) {
     var winner;
+    if (playerMove == 'rock' && computerMove == 'paper'){
+        winner = 'computer';
+    }  else if (playerMove == 'rock' && computerMove == 'rock'){
+        winner = 'tie';
+    }  else if (playerMove == 'rock' && computerMove == 'scissors'){
+        winner = 'player';
+    }  else if (playerMove == 'paper' && computerMove == 'rock'){
+        winner = 'player';
+    }  else if (playerMove =='paper' && computerMove == 'scissors'){
+        winner = 'computer';
+    }  else if (playerMove =='paper' && computerMove == 'paper'){
+        winner = 'tie';
+    }  else if (playerMove == 'scissors' && computerMove == 'scissors'){
+        winner = 'tie';
+    }  else if (playerMove =='scissors' && computerMove == 'rock'){
+        winner = 'computer';
+    }  else (playerMove == 'scissors' && computerMove == 'paper'){
+        winner = 'player';
+    }
+
+
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
